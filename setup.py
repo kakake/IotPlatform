64e9689bbe7e1467505c0407ceaafc36bde40014
kakake <343588387@qq.com>
2015-12-17 16:25:11 +0800

    all

---
+++ setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+config = {
+    'description': 'IotPlatform',
+    'author': 'kakake',
+    'url': 'www.efwplus.cn',
+    'download_url': 'www.efwplus.cn',
+    'author_email': '343588387@qq.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['iotserver'],
+    'scripts': [],
+    'name': 'iotserver'
+}
+
+
+setup(**config)
---
+++ tests/iotplatform_tests.py
@@ -0,0 +1,14 @@
+from nose.tools import *
+import iotserver
+
+
+def setup():
+    print "SETUP!"
+
+
+def teardown():
+    print "TEAR DOWN!"
+
+
+def test_basic():
+    print "I RAN!"
