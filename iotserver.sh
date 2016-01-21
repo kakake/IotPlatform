04d515444caf6758701786b0bc5164791afc0c19
kakake <343588387@qq.com>
2016-01-21 16:07:31 +0800

    a little

--- iotserver.sh
+++ iotserver.sh
@@ -6,33 +6,16 @@
 case "$1" in
   start)
         echo -n "Starting python: "
-        echo "---------------------------------------------------------------------------------" >>/var/log/python
-        date +"! %T %a %D : Starting python ." >>/var/log/python
-        echo "---------------------------------------------------------------------------------" >>/var/log/python
-	echo start
+        echo start
         cd  /root/IotPlatform
 	./run.py &
-	#sh /root/finmonAgent/startpy
         echo "Done."
-        echo ""
-        echo "---------------------------------------------------------------------------------" >>/var/log/python
-        date +"! %T %a %D : Finished." >>/var/log/python
-        echo "---------------------------------------------------------------------------------" >>/var/log/python
-        touch /var/lock/subsys/python
         ;;
   stop)
         echo -n "Shutting Down python Listeners: "
-        echo "---------------------------------------------------------------------------------" >>/var/log/python
-        date +"! %T %a %D : Shutting Down python." >>/var/log/python
-        echo "---------------------------------------------------------------------------------" >>/var/log/python
-	echo "python"
+        echo "python"
         killall -9 run.py
         echo "Done."
-        rm -f /var/lock/subsys/python      
-        echo "Done."
-        echo "---------------------------------------------------------------------------------" >>/var/log/python
-        date +"! %T %a %D : Finished." >>/var/log/python
-        echo "---------------------------------------------------------------------------------" >>/var/log/python
         ;;
   *)
   echo "Usage: python { start | stop | restart }"
